fd9c93ae02647707cf96a1cd64b502ae51ed047e
racchanak <racchana.professional@gmail.com>
2018-05-16 16:08:38 +0530

    First Commit .

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Git-Commands
 All Git Commands
+
+<b>CheckList</b>
+
+
+1. <code>Git Config Commands</code>
+2. <code>Basic Git Commands</code>
+3. <code>Branching Commands</code>
+4. <code>Merging Commands</code>
+5. <code>Resenting Commands</code>
+6. <code>Conflits Commands</code>
+
---
+++ configCommands.php
@@ -0,0 +1,14 @@
+<?php 
+/****
+ *  CONFIG
+ *  Configuring user information used across all local repositories
+ ***  git config --global user.name “[firstname lastname]”
+ **** git config --global user.name racchanak
+ *  set a name that is identifiable for credit when review version history
+ ***  git config --global user.email “[valid-email]”
+ **** git config --global user.email racchana.professional@gmail.com
+ *  set an email address that will be associated with each history marker
+ ***  git config --global color.ui auto
+ *  set automatic command line coloring for Git for easy reviewing
+****/
+?>
